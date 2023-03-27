0c29ab1936c3d6906709b9775e63a621a38a2f00
Akash Pratap Singh <akashpratapsingh01012000@gmail.com>
2023-03-28 01:15:46 +0530

    first challenge

---
+++ socialmedia website/index.js
@@ -0,0 +1,81 @@
+//SIDEBAR
+const menuItems = document.querySelectorAll('.menu-item')
+
+//MESSAGES
+const messagesNotification = document.querySelector('#messages-notifications');
+const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
+
+// Theme
+const theme = document.querySelector('#theme')
+const themeModal = document.querySelector('.customize-theme')
+
+
+
+//===========================SIDEBAR====================
+
+//remove active class from all menu items
+const changeActiveItem = () => {
+    menuItems.forEach(item => {
+        item.classList.remove('active');
+    })
+}
+
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        changeActiveItem();
+        item.classList.add('active');
+        if (item.id != 'notifications') {
+            document.querySelector('.notifications-popup').
+                style.display = 'none';
+        } else {
+            document.querySelector('.notifications-popup').
+                style.display = 'block';
+            document.querySelector('#notifications .notification-count').style.display = 'none';
+        }
+    })
+})
+
+
+//===============MESSAGES==============================
+
+// search chats
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
+    message.forEach(chat => {
+        let name = chat.querySelectorAll('h5').textContent.toLowerCase();
+        if (name.indexOf(val) != -1) {
+            chat.style.display = 'flex';
+        } else {
+            chat.style.display = 'none';
+        }
+    })
+}
+
+//search chat
+messageSearch.addEventListener('keyup', searchMessage);
+
+//highlight messages card when messages menu item is clicked 
+
+messagesNotification.addEventListener('click', () => {
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messagesNotification.querySelector('.notification-count').style.display = 'none';
+    setTimeout(() => {
+        messages.style.boxShadow = 'none';
+    }, 2000);
+})
+
+
+// theme customization 
+const openThemeModal = () => {
+    themeModal.style.display = 'grid';
+}
+theme.addEventListener('click', openThemeModal)
+
+
+
+
+
+
